bcd7cfcbee5f1468e2fe4962a4d6e173b5a4db27
Florent Huck <flovntp@gmail.com>
2023-01-12 14:58:33 +0100

    AppExtension: proper caching

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use App\Service\CommentHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class AppExtension extends AbstractExtension
 {
-    public function __construct(private CommentHelper $commentHelper, private $userStatuses = [])
+    public function __construct(private CommentHelper $commentHelper, private CacheInterface $cache)
     {
     }
 
@@ -22,11 +24,13 @@ class AppExtension extends AbstractExtension
 
     public function getUserActivityText(User $user): string
     {
-        if (!isset($this->userStatuses[$user->getId()])) {
-            $this->userStatuses[$user->getId()] = $this->calculateUserActivityText($user);
-        }
+        $key = sprintf('user_activity_text_%s', $user->getId());
 
-        return $this->userStatuses[$user->getId()];
+        return $this->cache->get($key, function(ItemInterface $item) use ($user) {
+            $item->expiresAfter(3600);
+
+            return $this->calculateUserActivityText($user);
+        });
     }
 
     private function calculateUserActivityText(User $user): string
